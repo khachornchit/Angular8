5f8c720b171c76f03f9c69c883401ba0fe680849
Khachornchit Songsaen <khachornchit.songsaen@gmail.com>
2019-10-11 22:21:30 +0700

    Created components completely

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PaymentDetailsComponent } from './payment-details/payment-details.component';
 import { PaymentDetailListComponent } from './payment-details/payment-detail-list/payment-detail-list.component';
+import { PaymentDetailComponent } from './payment-details/payment-detail/payment-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PaymentDetailsComponent,
-    PaymentDetailListComponent
+    PaymentDetailListComponent,
+    PaymentDetailComponent
   ],
   imports: [
     BrowserModule
